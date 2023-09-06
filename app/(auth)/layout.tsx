449b38e88d50cdeeacf7739c75b6912e62bfe877
devseryoon <sphere.sryn@gmail.com>
2023-09-06 12:12:42 +0900

    1.Clerk를 이용한 Auth(SignIn, SignOut등)적용 2. 기본 골격이 되는 레이아웃 적용

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+//SEO
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Thread",
+  description: "A Next.js 13 Mesta Threads App 제작",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
