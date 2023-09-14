75669e9b248f6f89a0a9532d6442bbe41c7fcfc1
devseryoon <sphere.sryn@gmail.com>
2023-09-14 17:47:28 +0900

    Thread card structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,41 @@
-import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+async function Home() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const result = await fetchPosts(1, 30);
+
   return (
-    <div>
-      {/* <UserButton afterSignOutUrl="/" /> */}
+    <>
       <h1 className="head-text text-left">HOME</h1>
-    </div>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result"> no threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
   );
 }
+export default Home;
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,12 +1,10 @@
 "use server";
-
 // /**
 // database actions through the browser side one reason is course cross origin requests.
 //  it doesn't allow it. that's why we develop apis and new servers.
 //  Databases are mostly server or API services.
 //  So this simple next.js 'use server directive' makes it all work
 // */
-
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -45,4 +43,35 @@ export async function createThread({
   } catch (error: any) {
     throw new Error(`Failed to create thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  //Skip할 포스트의 갯수 계산
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //Fetch the post that have no parents(top-level Threads)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        //recursion~~~~ 코멘트 갖고오고 싶으니까 ㅎㅅㅎ
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // 코멘트를 제외한 탑레벨 스레드만긁어온다.
+
+  const posts = await postsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+  return { posts, isNext };
 }
