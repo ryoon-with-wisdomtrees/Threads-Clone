fc62bdb1e0085dfda6c8b59758d4f380cdb64d43
devseryoon <sphere.sryn@gmail.com>
2023-09-16 18:09:44 +0900

    onboarding 관련 error 수정

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,15 @@
 import AccoutProfile from "@/components/forms/AccoutProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
-  const userInfo = {};
+  if (!user) return null;
+  const userInfo = await fetchUser(user?.id);
+  if (!userInfo) redirect("/");
+
   const userData = {
     id: user?.id || "",
     objectId: userInfo?._id,
